cacbd5d882a806474005d18d2535f15322724ec6
namangarg31 <namangarg040@gmail.com>
2023-09-19 01:57:55 +0530

    b1 merge into main

--- java-sample-git/src/B1.java
+++ java-sample-git/src/B1.java
@@ -1,6 +1,6 @@
 public class B1 {
     public String statement()
     {
-        return "This is class B";
+        return "This is class B used in the main branch";
     }
 }
--- java-sample-git/src/Main.java
+++ java-sample-git/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("In the main branch");
+        System.out.println("In the main branch");
+        System.out.println("In the main branch");
+        System.out.println("In the main branch");
     }
 }
